f08e05d105a9a198ea56ff5b99e0de6b72bc020b
Renze Post <renze.post@gmail.com>
2023-12-01 15:46:21 +0100

    Day 1

---
+++ day1/main.py
@@ -0,0 +1,16 @@
+import re
+
+
+def to_digits(line: str) -> int:
+    digits = re.findall(r"\d", line)
+    return int(f"{digits[0]}{digits[-1]}")
+
+
+def main():
+    with open("input.txt", "r") as file:
+        lines = file.readlines()
+        print(sum([to_digits(line) for line in lines]))
+
+
+if __name__ == "__main__":
+    main()
